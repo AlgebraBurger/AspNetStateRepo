0ba71ffe48b12eb42827e611608e896530d49a56
juliusbacosa <juliusbacosa@gmail.com>
2015-11-28 20:49:49 +0800

    - Init

---
+++ Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace State
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private int counter = 0;
+        protected int GetCounter()
+        {
+            Application.Lock();
+            int result = (int)(Application["counter"] ?? 0);
+            Application["counter"] = ++result;
+            Application.UnLock();
+            return result;
+        }
+    }
+}
---
+++ StateModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace State
+{
+    public enum City
+    {
+        London, Paris, Chicago
+    }
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public class StateModule : IHttpModule
+    {
+        public void Init(HttpApplication app)
+        {
+            app.PostAcquireRequestState += (src, args) => {
+                if (app.Context.Session != null
+                && app.Context.Session.IsNewSession
+                && !app.Context.Session.IsReadOnly)
+                {
+                    app.Context.Session["color"] = Color.Green;
+                    app.Context.Session["city"] = City.London;
+                }
+            };
+        }
+        public void Dispose()
+        {
+            // do nothing
+        }
+    }
+}
